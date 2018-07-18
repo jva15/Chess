6c1ca784a0bf414f8bd9ce3e565ebc0cff74dddf
jva15 <jva15@my.fsu.edu>
2018-07-18 14:31:27 -0400

    this class allows the creation of animated sprites

--- src/chess/Chara.java
+++ src/chess/Chara.java
@@ -3,28 +3,38 @@ package src.chess;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.Timer;
 
 public class Chara extends Actor {
 
 	public Chara() {
 		// TODO Auto-generated constructor stub
-	
-		 pixellength=27;//
-		 pixelheight=38;//
+		 super();
+		 pixellength=32;//
+		 pixelheight=32;//
 		 imagefileindex=1;//imagefileindex
 		 I_index_x=0;
 		 I_index_y=1;//sprite selectors
 		 //int[] iar={1,1,0,1,1,1,1,1,2,1,1,3};
 		 testanimation=new Animation( 500,new int[]{1,1,0,1,1,1,1,1,2,1,1,3});
+		 setOpaque(false);	
 	}
-	
 	
 	
 	Animation testanimation;
 	public Chara(Node cell, int fac) {
 		super(cell, fac);
-		// TODO Auto-generated constructor stub
+		pixellength=32;//
+		 pixelheight=32;//
+		 imagefileindex=1;//imagefileindex
+		 
+		 I_index_x=0;
+		 I_index_y=1;//sprite selectors
+		 //int[] iar={1,1,0,1,1,1,1,1,2,1,1,3};
+		 testanimation=new Animation( 500,new int[]{1,1,0,1,1,1,1,1,2,1,1,3});
+		 setOpaque(false);
+		 // TODO Auto-generated constructor stub
 	}
 	 	
 	
