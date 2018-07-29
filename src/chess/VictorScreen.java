ec87034c1d1e5d4ace67e2b79dd1fa2c522337f4
jva15 <jva15@my.fsu.edu>
2018-07-29 02:53:30 -0400

    added a victor JPanel

---
+++ src/chess/VictorScreen.java
@@ -0,0 +1,37 @@
+package src.chess;
+
+import java.awt.Graphics;
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class VictorScreen extends Sprite {
+	
+	
+	public VictorScreen(int player) {
+		//get the bounds
+		super();
+		pixellength=893;
+		pixelheight=94;
+		
+		//get the index
+		imagefileindex=2;
+		I_index_y=player;
+		I_index_x=0;
+		
+		//set size
+		xsize=publicdata.width/2;   //pixellength;
+		ysize=publicdata.height/10;
+		xoffset=publicdata.width/2;
+		yoffset=publicdata.height/2;
+		//setSize(xoffset, yoffset);
+		Active=true;
+		setOpaque(false);
+		setVisible(true);
+		
+		repaint();
+		//invalidate();
+	}
+
+
+}
